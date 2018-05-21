74df1d722bd947a90c7148e4a60b49f833b92025
John Paolo Paulino <paolo716@gmail.com>
2018-05-21 22:57:19 +0800

    Fix a few bugs for log out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   #get 'users/:username', to: 'users#show', as: 'user'
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get "signup", to:  'devise/registrations#new'
+  end
 
   get 'feed', to: 'feed#show'
-  
+
   resources :users, only: :show, param: :username do
     member do
       post 'follow', to: 'follows#create'
@@ -11,13 +18,8 @@ Rails.application.routes.draw do
   end
   resources :items
   resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-    get "signin", to: 'devise/sessions#new'
-    delete "signout", to: 'devise/sessions#destroy'
-    get "signup", to:  'devise/registrations#new'
-  end
+
+
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contacts', to: 'pages#contacts'
